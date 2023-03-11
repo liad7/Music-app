50a51d9961012a21a923c32b2aed77fee500941f
liad7 <liadgola7@gmail.com>
2023-03-12 00:23:41 +0200

    icon lib file

--- cmps/side-bar/nav-bar.tsx
+++ cmps/side-bar/nav-bar.tsx
@@ -1,12 +1,24 @@
 
 function NavBar() {
-  return (
-    <ul>
-        <li>Home</li>
-        <li>Search</li>
-        <li>Your Library</li>
-    </ul>
-  )
+    return (
+        <ul>
+            <li>
+                <a href="">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span>Search</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span>Your Library</span>
+                </a>
+            </li>
+        </ul>
+    )
 }
 
 export default NavBar
