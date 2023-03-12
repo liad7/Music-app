e72e63c6ffe94f84b0139cc655c03ebdd3d7b92b
liad7 <liadgola7@gmail.com>
2023-03-12 12:20:07 +0200

    logo and nav bar

--- cmps/side-bar/nav-bar.tsx
+++ cmps/side-bar/nav-bar.tsx
@@ -1,22 +1,25 @@
-import { HomeIcon } from "../../svg/icon-lib"
+import { HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid"
+import { QueueListIcon } from "@heroicons/react/24/outline"
 
 function NavBar() {
     return (
         <ul>
-            <li>
-                <a href="">
-                    <HomeIcon />
-                    <span>Home</span>
+            <li className="px-2 h-10">
+                <a href="" className="flex items-center gap-4 px-4">
+                    <HomeIcon className="h-6 w-6"/>
+                    <span className="text-sm">Home</span>
                 </a>
             </li>
-            <li>
-                <a href="">
-                    <span>Search</span>
+            <li className="px-2 h-10">
+                <a href="" className="flex items-center gap-4 px-4">
+                    <MagnifyingGlassIcon className="h-6 w-6"/>
+                    <span className="text-sm">Search</span>
                 </a>
             </li>
-            <li>
-                <a href="">
-                    <span>Your Library</span>
+            <li className="px-2 h-10">
+                <a href="" className="flex items-center gap-4 px-4">
+                    <QueueListIcon className="h-6 w-6 rotate-90"/>
+                    <span className="text-sm">Your Library</span>
                 </a>
             </li>
         </ul>
