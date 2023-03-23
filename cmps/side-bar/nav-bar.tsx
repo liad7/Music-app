e7511db4a6161230895d0fec3fb11ce1d937f821
liad7 <liadgola7@gmail.com>
2023-03-23 15:13:47 +0200

    hover on nav link

--- cmps/side-bar/nav-bar.tsx
+++ cmps/side-bar/nav-bar.tsx
@@ -5,19 +5,19 @@ function NavBar() {
     return (
         <ul className="font-bold mt-4 mb-6">
             <li className="flex items-center px-2 h-10">
-                <a href="" className="flex items-center gap-4 px-4">
+                <a href="" className="nav-link">
                     <HomeIcon className="h-6 w-6"/>
                     <span className="text-sm">Home</span>
                 </a>
             </li>
             <li className="flex items-center px-2 h-10">
-                <a href="" className="flex items-center gap-4 px-4">
+                <a href="" className="nav-link">
                     <MagnifyingGlassIcon className="h-6 w-6"/>
                     <span className="text-sm">Search</span>
                 </a>
             </li>
             <li className="flex items-center px-2 h-10">
-                <a href="" className="flex items-center gap-4 px-4">
+                <a href="" className="nav-link">
                     <QueueListIcon className="h-6 w-6 rotate-90"/>
                     <span className="text-sm">Your Library</span>
                 </a>
